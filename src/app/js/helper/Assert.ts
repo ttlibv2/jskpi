64ce81ba3c76f42433bb9ba5dc8c3ada026f5902
conyeu <tuanit.nguyenquoc@gmail.com>
2021-10-28 08:08:17 +0700

    Update Assert.ts

--- src/app/js/helper/Assert.ts
+++ src/app/js/helper/Assert.ts
@@ -3,20 +3,40 @@ import { Objects } from './Objects';
 
 export class Assert {
 	
+    /**
+     * @param {string} msg
+     * @throws Error
+     */
 	static fail(msg: string) {
 		throw new Error(msg);
 	}
 
+    /**
+     * @param {boolean} bool
+     * @param {string=} msg
+     * @throws Error
+     */
 	static isTrue(bool: boolean, msg?: string) {
 		if (!!bool) return bool;
 		else throw new Error(msg || `must be true`);
 	}
 
+    /**
+     * @param {boolean} bool
+     * @param {string=} msg
+     * @throws Error
+     */
 	static isFalse(bool: boolean, msg?: string): boolean {
 		if (!bool === true) return bool;
 		else throw new Error(msg || `must be false`);
 	}
 
+    /**
+     * @param {<T>} object
+     * @param {string=} msg
+     * @param {any} clsError
+     * @throws Error
+     */
 	static notNull<T>(object: T, msg?: string, clsError?: any): T {
 		if (Objects.notNull(object)) return object;
 		else {
@@ -25,11 +45,21 @@ export class Assert {
 		}
 	}
 
+    /**
+     * @param {<T[]>} object
+     * @param {string=} msg
+     * @throws Error
+     */
 	static allNotNull<T>(object: T[], msg?: string): T[] {
 		if (Objects.allNotNull(object)) return object;
 		else throw new Error(msg || 'must be not null');
 	}
 
+    /**
+     * @param {string} str
+     * @param {string=} msg
+     * @throws Error
+     */
 	static notEmpty(str: string, msg?: string): string {
 		if (Objects.notEmpty(str)) return str;
 		else throw new Error(msg || `must be not empty.`);
